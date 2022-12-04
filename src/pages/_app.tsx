f77d54fb38175df11dd10cc0add51c8490ec75b6
trentphoto <trentphoto@gmail.com>
2022-12-03 21:49:35 -0500

    chore: umami try 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,12 @@ import '@/styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
-        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
-          <Script
-            src='https://umami-production-2417.up.railway.app/umami.js'
-            data-website-id='04d5b531-82ba-4019-b6fb-74424aec66d0'
-            data-domains='secretariat.video'
-            strategy='lazyOnload'
-          />
-        )}
+      <Script
+        src='https://umami-production-2417.up.railway.app/umami.js'
+        data-website-id='04d5b531-82ba-4019-b6fb-74424aec66d0'
+        data-domains='secretariat.video'
+        strategy='lazyOnload'
+      />
       <Component {...pageProps} />
     </>
   );
